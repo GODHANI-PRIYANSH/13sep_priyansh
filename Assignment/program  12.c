8a3b3de9341954a287b1a2f268340becf9e65d7d
GODHANI-PRIYANSH <145126305+GODHANI-PRIYANSH@users.noreply.github.com>
2024-01-03 16:02:22 +0530

    Add files via upload

---
+++ Assignment/program  12.c
@@ -0,0 +1,20 @@
+//Write a program to find out the max number from given 10 elements of array?
+	#include <stdio.h>    
+         
+    int main()    
+    {    
+        //Initialize array     
+        int arr[] = {25, 11, 7, 41 , 37, 10, 6, 31, 5, 4 ,};      
+               
+        int length = sizeof(arr)/sizeof(arr[0]);       
+        int max = arr[0];    
+                
+        for (int i = 0; i < Max 10; i++);  
+		{     
+             
+           if(arr[i] > max)    
+               max = arr[i];    
+        }      
+        printf("Max 10 element in given array: %d\n", max);    
+        return 0;    
+    }    
